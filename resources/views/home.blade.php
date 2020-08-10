6adf60965f9622d458819d2f6118548de6bd6d6c
Davi Souto <davi.souto@gmail.com>
2020-08-10 20:48:44 -0300

    Fala 300 Ipsum

---
+++ resources/views/home.blade.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fala 300 Ipsum - O lorem ipsum do fala 300</title>
+
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Spartan&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Girassol&display=swap" rel="stylesheet">
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/fontawesome.min.js"></script>
+
+
+
+  <style type="text/css">
+    *{ font-family: Spartan, "Montserrat", Arial, sans-serif; }
+    html, body{ padding: 0; margin:0; background-color: #333; }
+    .container{ background-color: #333; padding: 0; margin: 0; }
+    .header{ height: 100vh; width: 100vw; background-color: #333; background-image: url('/images/header.jpg'); background-size: cover; }
+    .app{ height: 100vh; width; 100vw; background-color: #111; }
+    .footer{ background-color: rgba(204, 50, 39, 1); align-content: center; align-items: center; text-align: center; padding: 28px; color: #fff; }
+
+    .header-cover{ width: 100vw; height: 100vh; background-color: #333; position: absolute; left: 0; top: 0; opacity: 0.3; }
+    .header .header-left img{ padding: 22px; opacity: 0.11; transform: rotate(15deg); }
+    .header .header-right{ position: absolute; right: 0; top: 0; width: 40vw; height: 100vh; background-color: rgba(22,22,22,0.66); }
+    .header h1{ font-size: 60px; color: #fff; text-transform: uppercase; margin-top: 33vh; margin-left: 12%; background-color: rgba(204, 50, 39, 0.85); display: block; width: 390px; padding: 12px; border-radius: 0px; color: #fff; font-family: Girassol, Spartan, Montserrat; }
+    .header h2{ font-size: 16px; color: #fff; margin-left: 18%; opacity: 0.76; border-bottom: 2px solid rgba(204, 50, 39, 0.65); padding-bottom: 6px; }
+
+    .button-div{ padding: 22px; text-align: center; }
+    .button-div button{ color: #f9f9f9; padding: 19px 29px; background-color: rgba(204, 50, 39, 0.85); border: none; outline: none; font-size: 24px; transition: all 0.12s; cursor: pointer; }
+    .button-div button:hover, .button-div button:focus{ background-color: rgba(204, 50, 39, 0.95); }
+    .button-div button:active{ background-color: rgba(204, 50, 39, 1); color: #fff; }
+
+    #main-text{ width: 80vw; margin-left: 10vw; height: 70vh; border-radius: 0px; background-color: #222; border: 2px solid; border-color: transparent; outline: none; padding: 22px; color: #fff; font-size: 22px;  }
+    #main-text:focus, #main-text:active{ border-color: rgba(204, 50, 39, 0.45) !important; }
+
+    .footer .row{ margin: 8px 0px;}
+    .footer .row.icons{ font-size: 1.77em; }
+    .footer a, .footer a:focus, .footer a:visited, .footer a:active{ color: white; text-transform: none; text-decoration: underline;  }
+  </style>
+
+  <script>
+    var first = "Fala 300 ipsum";
+
+    var dictionary = [
+      "Eu, Tigas e os 300",
+      "Eu, Tigas e os 300",
+
+      "Salveeee",
+      "Salveeee",
+
+      "Não tinha um bunda mole",
+      "Só os cara brabo",
+      "Eae rapaziada, firmeza",
+      "Atenção 300",
+
+      "Auuuuuuhh",
+      "Auuuuuuhh",
+      "Hauuuuhh",
+      "Hauuuuhh",
+      
+      "Ohhh, Leonidas",
+      "Coisa minha aqui",
+      "Deixa eu perguntar pro cê",
+      "Essa guerra ai é contra quantos hein",
+      "Não que a gente já ta em 300 né",
+      "A pergunta é mais por negócio de curiosidade memo",
+      "Que a gente já vai a gente já vai né",
+      "É Leonidas, eu e o Tigas ta na dúvida aqui",
+      "A rapaziada ta eufórica ai",
+      "A Smart Fit fechô",
+      "A gente nem treinando tá",
+      "Queria saber ai",
+      "É contra quantos ai",
+      "Guerreiros",
+      "Se vocês quiserem chutar",
+      "Eu falo se ta perto ou não",
+      "Ok então",
+      "Se é 300 então",
+      "Numa dessa a gente ta indo forte né",
+      "300 contra o que, contra uns 150",
+
+      "Um pouco mais",
+      "Um pouco mais",
+      "Um pouco mais",
+      "Um pouco mais",
+
+      "Fala 300",
+      "Fala 300",
+      "Fala 300",
+      "Fala 300",
+      "Fala 300",
+      "Fala 300",
+
+      "Big, Fala 300",
+      "Big, Fala 300",
+      "Big, Fala 300",
+
+      "Quando é guerra é guerra",
+      "Geralmente é 300 contra 300",
+      "600 ?",
+      "2 pa cada né",
+      "Dalê aqui dalê ali",
+      "Vamo pa casa assistir netflix",
+      "Pouco mais o que Leonidas ?",
+      "Uns 1200 ?",
+      "Que ai já é quatro",
+      "Não sei nem se da",
+      "Éhh eu taméim...",
+      "Eu tameim não sei se da pa mim não",
+      "Hein Leonidas",
+      "Que se eu ando com ele",
+      "E pa ele nao da",
+      "Ja sobrou o deles pa mim ne",
+      "É Leonidas, não da não",
+      "Tem que ver ai quanto que é",
+      "Tem nenhuma média ai pa dividir por 2 ou dividir por 3 ?",
+      "Vô chuta alto hein Leonidas",
+      "É conta o que ?",
+      "Conta uns 3 mil ?",
+      "Orra Leonidas",
+      "Que ai já é 10 pá cada",
+      "Ta ligado",
+      "Ai tem que fazer girocóptero",
+      "Guerreiros",
+      
+      "Seremos 300 contra 30 mil Persas",
+      "Seremos 300 contra 30 mil Persas",
+      "Seremos 300 contra 30 mil Persas",
+      "Seremos 300 contra 30 mil Persas",
+      "Seremos 300 contra 30 mil Persas",
+
+      "Oh Leonidas",
+      "É conta quantos ?",
+      
+      "Oh Leonidas, cê tem que parar de arrumar essas confusão",
+      "Oh Leonidas, cê tem que parar de arrumar essas confusão",
+      
+      "Não da pa ir não",
+      "Cabei de noivá",
+    ];
+
+    const generate = function(pharagraphs = 3, words = 10){
+      var result = first + ". ";
+
+      for(var p = 1; p <= pharagraphs; p++)
+      {
+        for(var w = 1; w <= words; w++)
+        {
+          let item_key = Math.floor(Math.random() * dictionary.length)
+          let item = dictionary[item_key];
+          let more = "";
+
+          if (item.substr(-1) != "?")
+            more = ".";
+
+          result += item + more + " ";
+        }
+
+        if (p < pharagraphs)
+          result += "\n\n";
+      }
+
+      return result;
+    };
+
+    function generate_and_add(){
+      document.getElementById("main-text").value = generate();
+    }
+
+    window.onload = function(){
+      generate_and_add();
+    };
+  </script>
+</head>
+<body>
+  <div class="container">
+    <header class="header">
+      <div class="header-cover"></div>
+      <div class="header-left">
+        <img src="/images/code.webp" alt="Dev Icon" height="128" width="128">
+      </div>
+      <div class="header-right">
+        <h1>Fala 300 Ipsum</h1>
+        <h2>Cê tem que parar com esses ipsum...</h2>
+      </div>
+    </header>
+    
+    <div class="app">
+      <div class="button-div">
+        <button id="btn-generate" onclick="generate_and_add()">Fala 300 !</button>
+      </div>
+
+      <textarea name="" id="main-text" cols="30" rows="25"></textarea>
+    </div>
+    
+    <footer class="footer">
+      <div class="row">Developed by <a href="http://bitnary.com.br" target="_blank">Davi Souto</a></div>
+      <div class="row icons">
+        <a href="https://github.com/dvsouto" target="_blank"><i class="fa fa-github"></i></a>
+      </div>
+    </footer>
+  </div>
+</body>
+</html>
